9feda22e670cfdcb3a7b7781b91e3ccf83e9839b
Kovacevic Milenko <kummicko@gmail.com>
2023-02-06 12:55:46 +0100

    various updates

--- app/main/routes.py
+++ app/main/routes.py
@@ -15,5 +15,33 @@ def add():
     json_data = request.get_json()
     newTask = Todo(task=json_data.get('task'))
     db.session.add(newTask)
+    db.session.flush()
+    print(newTask.id)
+    db.session.commit()
+    return {'id' : newTask.id, 'timestamp': newTask.timestamp.strftime('%d.%m.%Y - %H:%M')}
+
+
+@main.put('/mark_done')
+def mark_done():
+    json_data = request.get_json()
+    markedTask = Todo.query.get_or_404(json_data.get('id'))
+    markedTask.complete = True
+    db.session.commit()
+    return json_data
+
+@main.put('/update_text')
+def update_text():
+    json_data = request.get_json()
+    taskToUpdate = Todo.query.get_or_404(json_data.get('id'))
+    taskToUpdate.task = json_data.get('task')
+    db.session.commit()
+    return json_data
+
+
+@main.delete('/delete')
+def delete():
+    json_data = request.get_json()
+    taskToDelete = Todo.query.get_or_404(json_data.get('id'))
+    db.session.delete(taskToDelete)
     db.session.commit()
     return json_data
